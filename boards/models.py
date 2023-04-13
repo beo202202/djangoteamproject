14b06455821256d23d85ea7d09e49c027dfc80ac
Lee Juneyoung <wnsdud318@gmail.com>
2023-04-13 20:00:10 +0900

    add settings.py media_url

--- boards/models.py
+++ boards/models.py
@@ -5,6 +5,7 @@ class Board(models.Model):
     # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     board_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
+
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
