03f617670f1459dcc058894b07ab822fde24d456
Songjimyung <116045723+Songjimyung@users.noreply.github.com>
2023-04-14 14:34:48 +0900

    Revert "Correct"

--- boards/models.py
+++ boards/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
 class Board(models.Model):
@@ -9,7 +8,6 @@ class Board(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True, default=None)
 
     class Meta:
         db_table = 'board'
